19031aafd7771a61445b3d00873a2cf4fc3b817d
seasons986 <annalin12345@gmail.com>
2023-09-04 00:11:18 -0400

    less phrases, styling

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ window.addEventListener("load", () => {
   }
 
   const container = document.getElementById("container");
-  const r = () => getRandomInt(7, 20);
+  const r = () => getRandomInt(5, 10);
 
   function displayText(t) {
     const p = document.createElement("p");
     p.innerHTML = t.substring(0, getRandomInt(1, t.length));
     p.style.marginTop = `${getRandomInt(0, 40)*10}px`;
     p.style.lineHeight = `${r()*2}px`;
+    p.style.letterSpacing = `${getRandomInt(0,7)}px`;
     container.appendChild(p);
   }
   
